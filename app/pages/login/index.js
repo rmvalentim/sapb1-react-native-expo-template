f6bca750bb7ef96d890f9743e5d27dc6b86dadcc
rmvalentim <rmoraesvalentim@gmail.com>
2025-06-10 14:20:33 -0300

    Keyboard quit increase in login

--- app/pages/login/index.js
+++ app/pages/login/index.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { StyleSheet, View, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
+import { StyleSheet, View, TouchableWithoutFeedback, KeyboardAvoidingView, ScrollView, Platform, Keyboard } from 'react-native'
 import { Appbar, TextInput, Button, HelperText, IconButton } from 'react-native-paper'
 import { authenticate as authenticateService } from '../../services/loginService';
 import { useAuth } from '../../../context/AuthContext';
@@ -66,98 +66,96 @@ export default function User(props) {
     }
 
     return (
-        <KeyboardAvoidingView
-            style={{ flex: 1 }}
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
-        >
-            <ScrollView>
-                <Appbar.Header>
-                    <Appbar.Content title="Login" />
-                </Appbar.Header>
-                <View style={styles.userContainer}>
-                    <TextInput
-                        mode="outlined"
-                        autoCapitalize="none"
-                        label="User"
-                        style={styles.textInput}
-                        value={defaultUser}
-                        disabled={true}
-                    />
-                    <TextInput
-                        mode="outlined"
-                        autoCapitalize="none"
-                        label="Password"
-                        style={styles.textInput}
-                        secureTextEntry={true}
-                        value={password}
-                        onChangeText={(text) => {
-                            setPassword(text)
-                        }}
-                    />
-                    <Button
-                        mode="contained"
-                        style={styles.button}
-                        onPress={authenticate}>
-                        Login
-                    </Button>
-                    <HelperText type="error" visible={hasError}>
-                        {errorMessage}
-                    </HelperText>
-                    <IconButton
-                        icon="cog"
-                        size={30}
-                        mode='contained'
-                        style={styles.settingsButton}
-                        onPress={() => setIsSettingsVisible(!isSettingsVisible)}
-                    />
-                    {isSettingsVisible && (
-                        <View>
-                            <TextInput
-                                mode="outlined"
-                                autoCapitalize="none"
-                                label="Default User"
-                                style={styles.textInput}
-                                value={defaultUser}
-                                onChangeText={(text) => {
-                                    setDefaultUser(text)
-                                }}
-                            />
-                            <TextInput
-                                mode="outlined"
-                                autoCapitalize="none"
-                                label="Service Layer URL"
-                                style={styles.textInput}
-                                value={serviceLayerURL}
-                                onChangeText={(text) => {
-                                    setServiceLayerURL(text)
-                                }}
-                            />
-                            <TextInput
-                                mode="outlined"
-                                autoCapitalize="none"
-                                label="Company DB"
-                                style={styles.textInput}
-                                value={companyDb}
-                                onChangeText={(text) => {
-                                    setCompanyDb(text)
-                                }}
-                            />
-                            <Button
-                                mode="contained"
-                                style={styles.button}
-                                onPress={saveSettings}>
-                                Save Settings
-                            </Button>
-                        </View>
-                    )}
-                </View>
-            </ScrollView>
-
-        </KeyboardAvoidingView>
-
-
-
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <KeyboardAvoidingView
+                style={{ flex: 1 }}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
+            >
+                <ScrollView>
+                    <Appbar.Header>
+                        <Appbar.Content title="Login" />
+                    </Appbar.Header>
+                    <View style={styles.userContainer}>
+                        <TextInput
+                            mode="outlined"
+                            autoCapitalize="none"
+                            label="User"
+                            style={styles.textInput}
+                            value={defaultUser}
+                            disabled={true}
+                        />
+                        <TextInput
+                            mode="outlined"
+                            autoCapitalize="none"
+                            label="Password"
+                            style={styles.textInput}
+                            secureTextEntry={true}
+                            value={password}
+                            onChangeText={(text) => {
+                                setPassword(text)
+                            }}
+                        />
+                        <Button
+                            mode="contained"
+                            style={styles.button}
+                            onPress={authenticate}>
+                            Login
+                        </Button>
+                        <HelperText type="error" visible={hasError}>
+                            {errorMessage}
+                        </HelperText>
+                        <IconButton
+                            icon="cog"
+                            size={30}
+                            mode='contained'
+                            style={styles.settingsButton}
+                            onPress={() => setIsSettingsVisible(!isSettingsVisible)}
+                        />
+                        {isSettingsVisible && (
+                            <View>
+                                <TextInput
+                                    mode="outlined"
+                                    autoCapitalize="none"
+                                    label="Default User"
+                                    style={styles.textInput}
+                                    value={defaultUser}
+                                    onChangeText={(text) => {
+                                        setDefaultUser(text)
+                                    }}
+                                />
+                                <TextInput
+                                    mode="outlined"
+                                    autoCapitalize="none"
+                                    label="Service Layer URL"
+                                    style={styles.textInput}
+                                    value={serviceLayerURL}
+                                    onChangeText={(text) => {
+                                        setServiceLayerURL(text)
+                                    }}
+                                />
+                                <TextInput
+                                    mode="outlined"
+                                    autoCapitalize="none"
+                                    label="Company DB"
+                                    style={styles.textInput}
+                                    value={companyDb}
+                                    onChangeText={(text) => {
+                                        setCompanyDb(text)
+                                    }}
+                                />
+                                <Button
+                                    mode="contained"
+                                    style={styles.button}
+                                    onPress={saveSettings}>
+                                    Save Settings
+                                </Button>
+                            </View>
+                        )}
+                    </View>
+                </ScrollView>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     )
 }
 
